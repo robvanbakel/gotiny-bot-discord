b087ff92c7018f6f09d53654566e8b7baa4d03ca
Rob van Bakel <robbakel@gmail.com>
2021-08-24 08:50:16 +0200

    Working version

---
+++ helpers.js
@@ -0,0 +1,16 @@
+// Check for valid URL
+const urlCheck = str => {
+  const regex = new RegExp('(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?', 'gi'); // fragment locator
+
+    const matches = str.match(regex)
+
+   return matches
+  
+}
+
+module.exports = {urlCheck}
