137daa83f861a2fbd5a2d1ce2ab9800711c4de6d
Rob van Bakel <robbakel@gmail.com>
2022-01-29 23:47:36 +0100

    Reformat files

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 const Discord = require('discord.js')
-const gotiny = require("gotiny")
+const gotiny = require('gotiny')
 require('dotenv').config()
 
 const client = new Discord.Client()
 
 const { urlCheck } = require('./helpers')
 
-client.on('ready', () => { console.log(`Discord bot: Logged in as ${client.user.tag}!`) });
+client.on('ready', () => {
+  console.log(`Discord bot: Logged in as ${client.user.tag}!`)
+})
 
 // Listens for every message
-client.on('message', msg => {
-  
+client.on('message', (msg) => {
   // Check for valid links
-  const url = urlCheck(msg.content);
+  const url = urlCheck(msg.content)
 
   // Fires on every link longer than 24 characters
   if (!msg.author.bot && url && url[0].length > 24) {
-
     // Get GoTiny link and sends the response in the channel
-    gotiny.set(url[0])
-      .then(res => client.channels.cache.get(msg.channel.id).send(res[0].tiny))
-      .catch(err => console.log(err))
+    gotiny
+      .set(url[0])
+      .then((res) => client.channels.cache.get(msg.channel.id).send(res[0].tiny))
+      .catch((err) => console.log(err))
+  }
+})
 
-  };
-
-});
-
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN)
